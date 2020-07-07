fc9511a923431f4c07fecb09980748978590373c
nabil <nabil@kreezalid.co>
2020-07-07 11:14:52 +0200

    Delete webhooks is now handled

--- webhooks.php
+++ webhooks.php
@@ -1,34 +1,49 @@
 <?php
 require 'vendor/autoload.php';
 
-if (
-    !empty($_POST)
-    && ($_POST['type'] == 'user.creation' || $_POST['type'] == 'user.update')
-) {
+if (empty($_POST)){
+    die('No posted data');
+}
 
-    $api = new \Kreezalid\KreezalidApi();
-    $api->Config->setApiKey('kreezalid-api-key');
-    $api->Config->setApiSecret('kreezalid-api-secret');
-    $api->Config->setApiUrl('kreezalid-api-url');
+// We need Algolia client to update Algolia Index
+$client = Algolia\AlgoliaSearch\SearchClient::create(
+    'algolia-app-id',
+    'algolia-app-key'
+);
 
-    try {
+try {
+    if ($_POST['type'] === 'user.creation' || $_POST['type'] === 'user.update') {
+
+        // Let's fetch the fresh data from Kreezalid API to have the most recent data
+        $api = new \Kreezalid\KreezalidApi();
+        $api->Config->setApiKey('kreezalid-api-key');
+        $api->Config->setApiSecret('kreezalid-api-secret');
+        $api->Config->setApiUrl('kreezalid-api-url');
+
         $user = $api->Users->get($_POST['data']['id'], [
-            'fields' => 'id,public_name,avatar,attributes,group_id,profile_url',
+            // Select only relevant fields
+            'fields' => 'id,public_name,avatar,attributes,group_id,profile_url'
         ]);
 
         if (is_object($user) && isset($user->id)) {
-
-            $client = Algolia\AlgoliaSearch\SearchClient::create(
-                'algolia-app-id',
-                'algolia-app-key'
-            );
-
+            /**
+             * Use Algolia methods to update the index
+             * https://www.algolia.com/doc/api-reference/api-methods/save-objects/
+             */
             $index = $client->initIndex('algolia-index');
+            // $user is an object. Algolia works only with Arrays
             $res = $index->saveObject((array)$user, ['objectIDKey' => 'id']);
         }
-    } catch (\Kreezalid\Librairies\Exception $e) {
-        die($e);
+
+    } else if ($_POST['type'] === 'user.delete') {
+        /**
+         * Use Algolia methods to delete the object
+         * https://www.algolia.com/doc/api-reference/api-methods/delete-objects/
+         */
+        $index = $client->initIndex('algolia-index');
+        $res = $index->deleteObject($_POST['data']['id']);
     }
+} catch (\Kreezalid\Librairies\Exception $e) {
+    die($e);
 }
 
-// TODO: user delete
